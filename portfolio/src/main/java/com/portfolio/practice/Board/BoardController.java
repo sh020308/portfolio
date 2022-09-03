35fb40fa60182aac2234fd3e190e6c9d03ad2777
sh020308 <sh020307@naver.com>
2022-09-03 12:51:16 +0900

    Initial commit

---
+++ portfolio/src/main/java/com/portfolio/practice/Board/BoardController.java
@@ -0,0 +1,14 @@
+package com.portfolio.practice.Board;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	@GetMapping("/list")
+	public String list() {
+		return "board/list";
+	}
+}
---
+++ portfolio/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,10 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>테스트</title>
+	</head>
+	<body>
+		<div>테스트확인</div>
+	</body>
+</html>
