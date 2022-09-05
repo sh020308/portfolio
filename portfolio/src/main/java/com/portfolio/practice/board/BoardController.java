39f3197e7e500d0e3a50870f74f31f581c19c9eb
sh020308 <sh020307@naver.com>
2022-09-05 22:42:39 +0900

    09-05

--- portfolio/src/main/java/com/portfolio/practice/board/BoardController.java
+++ portfolio/src/main/java/com/portfolio/practice/board/BoardController.java
@@ -1,4 +1,4 @@
-package com.portfolio.practice.Board;
+package com.portfolio.practice.board;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
---
+++ portfolio/src/main/java/com/portfolio/practice/board/BoardMapper.java
@@ -0,0 +1,5 @@
+package com.portfolio.practice.board;
+
+public class BoardMapper {
+
+}
